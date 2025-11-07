9949c09600053f5139c761cdc5747591a25860d1
Andy Snazyk <asnazyk@AO.local>
2025-11-07 11:29:31 -0500

    fix: remove sign-in/sign-up routes entirely and purge Clerk

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,2 +0,0 @@
-import {SignIn} from '@clerk/nextjs'
-export default function P(){return(<main className='main section'><div className='card p-6'><SignIn routing='path' path='/sign-in'/></div></main>)}
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,2 +0,0 @@
-import {SignUp} from '@clerk/nextjs'
-export default function P(){return(<main className='main section'><div className='card p-6'><SignUp routing='path' path='/sign-up'/></div></main>)}
